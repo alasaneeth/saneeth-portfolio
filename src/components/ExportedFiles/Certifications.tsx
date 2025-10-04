9c56db81a4605d4ec76f5a4fead23b85bc03ac9d
alasaneeth <alasaneeth22@gmail.com>
2025-10-04 22:47:22 +0300

    Feat: Update Crtifications

--- src/components/ExportedFiles/Certifications.tsx
+++ src/components/ExportedFiles/Certifications.tsx
@@ -1,60 +1,130 @@
 import microsoft from "../../assets/CertificationImages/microsoft.png";
-
+import linkedin from "../../assets/CertificationImages/linkedin.jpg"
+import hackerRank from "../../assets/CertificationImages/HackerRank.png"
+import Udemy from "../../assets/CertificationImages/udemy.png"
 
 export const CERTIFICATIONS = [
-//Azure
-       {
-   
-      title: "Describe the benefits of using cloud services",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/7knkfvbz",
-    },
+  {
+    provider: "microsoft",
+    items: [
       {
-   
-      title: "Describe the benefits of using cloud services",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/w269plsn",
-    },
+        title: "Describe the benefits of using cloud services",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/7knkfvbz",
+      },
       {
-   
-      title: "Describe cloud service types",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/xqu8kdcy",
-    },
+        title: "Describe cloud service types",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/w269plsn",
+      },
+      {
+        title: "Describe cloud computing",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/xqu8kdcy",
+      },
+      {
+        title: "Branch the flow of code using the switch-case construct in C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/yef72hpr",
+      },
+      {
+        title: "Add decision logic to your code using if, else, and else if statements in C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/7kywgcgz",
+      },
+      {
+        title: "Call methods from the .NET Class Library using C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/h79lvx78",
+      },
+      {
+        title: "Store and iterate through sequences of data using Arrays and the foreach statement in C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/api/achievements/share/en-us/AasifSaneeth-5418/8ZUS2BZW?sharingId=95C0CC75F5920A85",
+      },
+      {
+        title: "Get started with classes and objects",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/api/achievements/share/en-us/AasifSaneeth-5418/E575R3CP?sharingId=95C0CC75F5920A85",
+      },
+      {
+        title: "Guided project - Calculate and print student grades",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/k624gthb",
+      },
+      {
+        title: "Perform basic string formatting in C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/ur5w7ls3",
+      },
+      {
+        title: "Store and retrieve data using literal and variable values in C#",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/xqu8kdcy",
+      },
+      {
+        title: "Write your first C# code",
+        logo: microsoft,
+        url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/w269kdjn",
+      },
+    ]
+  },
+  {
+     provider: "HackerRank",
+     items: [
+      {
+         title: "React(Basic)",
+          logo: hackerRank,
+          url:"https://www.hackerrank.com/certificates/iframe/5dd4a8b6f3e4"
+      },
        {
-   
-      title: "Describe cloud computing",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/xqu8kdcy",
-    },
-    //Start C#.ner
-    {
-      title: "Get started with classes and objects",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/api/achievements/share/en-us/AasifSaneeth-5418/E575R3CP?sharingId=95C0CC75F5920A85",
-    },
+         title: "SQL(Basic)",
+          logo: hackerRank,
+          url:"https://www.hackerrank.com/certificates/iframe/246cde45b342"
+      },
+         {
+         title: "SQL(InterMediate)",
+          logo: hackerRank,
+          url:"https://www.hackerrank.com/certificates/iframe/246cde45b342"
+      }
+     ]
+  },
 
-    {
-      title: "Guided project - Calculate and print student grades",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/k624gthb",
-    },
-    {
-    
-      title: "Perform basic string formatting in C#",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/ur5w7ls3",
-    },
-    {
-      
-      title: "Store and retrieve data using literal and variable values in C#",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/xqu8kdcy",
-    },
+  {
+    provider: "linkedin",
+    items: [
       {
-      title: "Write your first C# code",
-      logo: microsoft,
-      url: "https://learn.microsoft.com/en-us/users/aasifsaneeth-5418/achievements/w269kdjn",
-    },
- 
-  ];
+        title: "Microservices Foundations",
+        logo: linkedin,
+        url: "https://www.linkedin.com/learning/certificates/ea28460ccafd643f2796307c6ec95486e36dd5d6333a49970fa47373cbffcbc3?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BH%2BY1WLbtTTK3ea%2F9BZ6aBw%3D%3D",
+      },
+      {
+        title: "Building RESTful APIs with Node.js and Express",
+        logo: linkedin,
+        url: "https://www.linkedin.com/learning/certificates/2379ae5ababe3492eeaea523a1940746ad29ad4e5a87c63b7713e60d3a2570c6?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BH%2BY1WLbtTTK3ea%2F9BZ6aBw%3D%3D",
+      },
+       {
+        title: "Learning Docker",
+        logo: linkedin,
+        url: "https://www.linkedin.com/learning/certificates/234d45350d5834b253ca23cccdc87aee21e190b5084af1a1c6c982db52859abf?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BH%2BY1WLbtTTK3ea%2F9BZ6aBw%3D%3D",
+      },
+      {
+        title: "Node.js Essential Training",
+        logo: linkedin,
+        url:"https://www.linkedin.com/learning/certificates/f65874558c9a0708c985e81a4068f72aa205b7f2d9c8346920381e9584432375?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BH%2BY1WLbtTTK3ea%2F9BZ6aBw%3D%3D"
+
+      }
+      // Add more LinkedIn certificates here
+    ]
+  },
+    {
+      provider: "Udemy",
+      items:[
+        {
+          title: "Ultimate Guide to .Net (8) Core Web API with EF Core",
+          logo:Udemy,
+          url: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-5209f443-a477-4a51-9632-6456207297d3.pdf"
+        }
+      ]
+  },
+];
